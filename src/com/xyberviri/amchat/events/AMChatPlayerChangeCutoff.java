50c150527428000604f3ddb2bc564c6027d5bb68
Xyberviri <xyberviri@gmail.com>
2012-04-29 21:12:56 -0500

    Removed unused import

--- src/com/xyberviri/amchat/events/AMChatPlayerChangeCutoff.java
+++ src/com/xyberviri/amchat/events/AMChatPlayerChangeCutoff.java
@@ -1,7 +1,6 @@
 package com.xyberviri.amchat.events;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.HandlerList;
 
 public class AMChatPlayerChangeCutoff extends AMChatPlayerChangeCode {
 	public AMChatPlayerChangeCutoff(Player player,int varValue) {
--- src/com/xyberviri/amchat/events/AMChatPlayerChangeFrequency.java
+++ src/com/xyberviri/amchat/events/AMChatPlayerChangeFrequency.java
@@ -1,7 +1,6 @@
 package com.xyberviri.amchat.events;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.HandlerList;
 
 public class AMChatPlayerChangeFrequency extends AMChatPlayerChangeCode  {
 	public AMChatPlayerChangeFrequency(Player player,int varValue) {
